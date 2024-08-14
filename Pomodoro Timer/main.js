8f076f7837d09c960ba317dfe01367ad0230ef4d
Cesar Pinero <cpinero522003@gmail.com>
2024-08-14 21:01:50 +0800

    1st project with git

---
+++ Pomodoro Timer/main.js
@@ -0,0 +1,36 @@
+const startingMinutes = 2;
+let time = startingMinutes * 60;
+let isRunning = false; 
+const timerEl = document.querySelector("#timer");
+
+
+function start() {
+    if(!isRunning){
+        let intervalId = setInterval(() => {
+            isRunning = true
+            let minutes = Math.floor(time / 60);
+            let seconds = time % 60;
+        
+            seconds = seconds < 10 ? '0' + seconds : seconds;
+        
+            timerEl.innerHTML = `${minutes}: ${seconds}`;
+            time--;
+            i
+            if (time == 0) {
+              clearInterval(intervalId);
+              alert("Time's up");
+            }
+          }, 1000);
+    }
+}
+
+function stop(){
+    clearInterval(intervalId);
+    isRunning = false;
+}
+
+function reset(){
+    clearInterval(intervalId);
+    time = startingMinutes * 60;
+    timerEl.innerHTML = `${startingMinutes}: 00`;
+}
