1871a4df3947335868c583e09a619356d1b5a501
Cesar Pinero <cpinero522003@gmail.com>
2024-09-04 22:25:13 +0800

    one piece website

--- Navbar/main.js
+++ Navbar/main.js
@@ -24,3 +24,35 @@ const sidebarAppear = () => {
 menuBtn.addEventListener("click", sidebarAppear);
 sidebarMenuBtn.addEventListener("click", sidebarAppear);
 
+
+const navi = document.querySelector("#nav");
+const home = document.querySelector(".home");
+
+
+window.addEventListener("scroll", function(){
+    if (window.scrollY > home.offsetTop + 100) {
+        navi.style.opacity = 0.5;
+      } else {
+        navi.style.opacity = 1; //
+      }
+});
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting) {
+            entry.target.classList.add("show-left-right");
+
+        } else {
+            entry.target.classList.remove("show-left-right");
+        }
+    });
+});
+
+
+
+const hiddenLeftElement = document.querySelectorAll(".hidden-left");
+const hiddenRightElement = document.querySelectorAll(".hidden-right");
+hiddenLeftElement.forEach((el) => observer.observe(el));
+hiddenRightElement.forEach((el) => observer.observe(el));
